9a2d5e65ac96b6ec99ad4ffe793ad98745dfb0b4
0xf1f0 <oobey1@aol.com>
2018-08-10 23:57:25 -0400

    Code clean up

--- Zenith Fitness/Zenith Fitness/MemberAccount.aspx.cs
+++ Zenith Fitness/Zenith Fitness/MemberAccount.aspx.cs
@@ -12,32 +12,28 @@ namespace Zenith_Fitness
             HttpContext.Current.Response.Cache.SetCacheability(HttpCacheability.NoCache);
             HttpContext.Current.Response.Cache.SetNoServerCaching();
             HttpContext.Current.Response.Cache.SetNoStore();
-            if (Session["Username"] != null)
-            {
-                currentUser.Text = Session["Username"].ToString();
-                var memId = int.Parse(Session["mem_id"].ToString());
-                const string cmd = "SELECT [member_id], [membership_name] ,[membership_status], [membership_start], [membership_end] FROM[dbo].[Membership] WHERE [member_id] = @memID;";
+            if (Session["Username"] == null)
+                Response.Redirect("MemberLogin.aspx");
 
-                using (var userAcct = new SqlConnection(SqlDataSource1.ConnectionString))
+            currentUser.Text = Session["Username"].ToString();
+            var memId = int.Parse(Session["mem_id"].ToString());
+            const string cmd = "SELECT [member_id], [membership_name] ,[membership_status], [membership_start], [membership_end] FROM[dbo].[Membership] WHERE [member_id] = @memID;";
+
+            using (var userAcct = new SqlConnection(SqlDataSource1.ConnectionString))
+            {
+                userAcct.Open();
+                using (var checkUser = new SqlCommand(cmd, userAcct))
                 {
-                    userAcct.Open();
-                    using (var checkUser = new SqlCommand(cmd, userAcct))
+                    checkUser.Parameters.AddWithValue("@memID", memId);
+                    var reader = checkUser.ExecuteReader();
+                    while (reader.Read())
                     {
-                        checkUser.Parameters.AddWithValue("@memID", memId);
-                        var reader = checkUser.ExecuteReader();
-                        while (reader.Read())
-                        {
-                            lblPlan.Text = reader["membership_name"].ToString();
-                            lblStatus.Text = reader["membership_status"].ToString();
-                            lblStart.Text = reader["membership_start"].ToString();
-                            lblEnd.Text = reader["membership_end"].ToString();
-                        }
+                        lblPlan.Text = reader["membership_name"].ToString();
+                        lblStatus.Text = reader["membership_status"].ToString();
+                        lblStart.Text = reader["membership_start"].ToString();
+                        lblEnd.Text = reader["membership_end"].ToString();
                     }
                 }
-            }
-            else
-            {
-                Response.Redirect("MemberLogin.aspx");
             }
         }
 
